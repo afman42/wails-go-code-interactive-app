bf6a45927af302d49a5dde9e76b7d5f58313fb31
Afif Arman <afifarman50@gmail.com>
2025-06-16 04:18:37 +0700

    add list numbering

--- frontend/src/utils/lang-state.svelte.ts
+++ frontend/src/utils/lang-state.svelte.ts
@@ -7,9 +7,9 @@ export let langState = $state({
       go: 'package main\n\nimport "fmt"\n\nfunc main(){\n\tfmt.Println("hello world")\n}\n\n\n\n\n\n\n\n\n\n'
     },
     stq: {
-      node: "//Don't remove function intIntoString\nfunction intIntoString(n){\n\treturn\n}\n\n",
+      node: "//Don't remove function intIntoString\nfunction intIntoString(n){\n\treturn\n}\n\n\n\n\n\n\n\n\n\n\n\n\n",
       php: "<?php\n\n//Don't remove function intIntoString\nfunction intIntoString($n){\n\treturn\n}\n\n",
-      go: 'package main\n\nimport (\n\t"fmt" //Don"t remove\n\t"strconv"  //Don"t remove\n)\n\n//Don"t remove function intIntoString\nfunc intIntoString(n int) string {\n\treturn\n}\n\n'
+      go: 'package main\n\nimport (\n\t"fmt" //Don"t remove\n\t"strconv"  //Don"t remove\n)\n\n//Don"t remove function intIntoString\nfunc intIntoString(n int) string {\n\treturn\n}\n\n\n\n\n\n'
     }
   },
   type: 'repl'
