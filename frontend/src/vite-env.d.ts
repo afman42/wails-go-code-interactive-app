098ac6fa5209de0498bd13cc0583b0212a77be07
afman42 <afman42@afman42.afman42>
2025-06-21 14:39:46 +0700

    remove unused code

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,18 +1,2 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-
-import { main } from '../wailsjs/go/models'
-
-type ResultData = main.ResponseData & {
-  meta: {
-    status_code: number
-    message: string
-  }
-  data: {
-    txt: string
-    out: string
-    errout: string
-    lang: string
-    type: string
-  }
-}
